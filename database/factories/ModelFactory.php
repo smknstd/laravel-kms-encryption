ad32486b454952c53435955729cb26c323e09eb9
Arnaud Becher <arnaud.becher@gmail.com>
2021-09-03 16:34:27 +0200

    initial commit

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace VendorName\Skeleton\Database\Factories;
+namespace Smknstd\LaravelKmsEncryption\Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 
--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace VendorName\Skeleton\Tests;
+namespace Smknstd\LaravelKmsEncryption\Tests;
 
 class ExampleTest extends TestCase
 {
